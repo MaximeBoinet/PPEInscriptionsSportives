d66bf388af761ed22b089534ed6b9a6f448e0959
AureLaz <aurelien.lazaro@gmail.com>
2016-02-18 15:15:54 +0100

    Test Done
    Test fait mais certains sont à revoir, quelques erreurs subsistent.

---
+++ src/inscriptions/EquipeTest.java
@@ -0,0 +1,36 @@
+package inscriptions;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import java.util.Set;
+import inscriptions.Inscriptions;
+
+public class EquipeTest {
+	Inscriptions inscriptionTest = Inscriptions.getInscriptions();
+	Equipe equipeTest = inscriptionTest.createEquipe("L'EQUIPE TEST");
+	Personne personneTest = inscriptionTest.createPersonne("TEST", "test","tTEST@gmail.com");
+
+	@Test
+	public void testGetMembres() {
+		Set<Personne> setMembresTest = equipeTest.getMembres();
+		equipeTest.add(personneTest);
+		assertEquals(setMembresTest, equipeTest.getMembres());
+	}
+
+	@Test
+	public void testAddPersonne() {
+		Set<Personne> setMembresTest = equipeTest.getMembres();
+		equipeTest.add(personneTest);
+		assertTrue(setMembresTest.contains(personneTest));
+	}
+
+	@Test
+	public void testRemovePersonne() {
+		Set<Personne> setMembresTest = equipeTest.getMembres();
+		equipeTest.add(personneTest);
+		equipeTest.remove(personneTest);
+		assertFalse(setMembresTest.contains(personneTest));
+	}
+
+}
